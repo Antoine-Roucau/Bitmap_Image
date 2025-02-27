306a7c13195d8b3480e3b7462b78c668a3ccf50a
Antoine Roucau <antoine.roucau@gmail.com>
2025-02-27 14:14:31 +0100

    passage net 9

--- Main.cs
+++ Main.cs
@@ -7,7 +7,7 @@
         static void Main(string[] args)
         {
             
-            MyImage image = new MyImage("bin/Debug/net7.0/Image/coco.bmp");
+            MyImage image = new MyImage("bin/Debug/net7.0/Image/coco");
             
             image.From_Image_To_File("cocoRevive");
 
